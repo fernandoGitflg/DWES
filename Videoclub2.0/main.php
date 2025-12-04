c3b0b3492bf3a70b6f27cc1dd3e74998c06cba07
fernandoGitflg <feymarlopga@gmail.com>
2025-12-04 13:06:20 +0100

    mas cambios del proyecto

--- Videoclub2.0/main.php
+++ Videoclub2.0/main.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-include_once 'app/Dwes/ProyectoVideoclub/Videoclub.php';
+include_once 'Dwes/ProyectoVideoclub/Videoclub.php';
 
 use Dwes\ProyectoVideoclub\Videoclub;
 
