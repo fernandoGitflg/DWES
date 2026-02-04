2103dc3bc55d060452181668653da7d133a140fc
fernandoGitflg <feymarlopga@gmail.com>
2026-02-04 11:33:53 +0100

    Update hasheador.php

--- hasheador.php
+++ hasheador.php
@@ -1,10 +1,10 @@
 <?php
-$clave = '1111';
+$clave = '1234';
 $hash = password_hash($clave, PASSWORD_DEFAULT);
 echo $hash;
 
 //$hash = '$2y$10$rn5BTva2jKac4asgw3qCXem0v0lZ1YB/virqorzgnJu8e/8p2NZzK';
-$input = '1111';
+$input = '1234';
 
 if (password_verify($input, $hash)) {
     echo "Coincide";
